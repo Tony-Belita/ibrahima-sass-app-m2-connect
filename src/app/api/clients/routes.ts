0e7bbb79d19850af2bac159b95da816c59f6c74e
Tony-Belita <barryibrahimatalibe@gmail.com>
2025-08-11 12:01:35 +0200

    routes client

---
+++ src/app/api/clients/routes.ts
@@ -0,0 +1,53 @@
+import { supprimerClient, ajouterClient, getClients } from "@/db/actions";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+	const { userID, customerName, customerEmail, customerAddress } =
+		await req.json();
+
+	try {
+		await ajouterClient({
+			user_id: userID,
+			name: customerName,
+			email: customerEmail,
+			address: customerAddress,
+		});
+		return NextResponse.json(
+			{ message: "Nouveau client créé !" },
+			{ status: 201 }
+		);
+	} catch (err) {
+		return NextResponse.json(
+			{ message: "Une erreur s'est produite", err },
+			{ status: 400 }
+		);
+	}
+}
+
+export async function GET(req: NextRequest) {
+   const userID = req.nextUrl.searchParams.get("userID");
+    
+	try {
+		const clients = await getClients(userID!);
+        return NextResponse.json({ message: "Clients récupérés avec succès !", clients }, { status: 200 });
+	} catch (err) {
+		return NextResponse.json(
+			{ message: "Une erreur s'est produite", err },
+			{ status: 400 }
+		);
+	}
+}
+
+export async function DELETE(req: NextRequest) {
+	const clientID = req.nextUrl.searchParams.get("id");
+
+	try {
+		await supprimerClient(Number(clientID));
+		return NextResponse.json({ message: "Client supprimé !" }, { status: 200 });
+	} catch (err) {
+		return NextResponse.json(
+			{ message: "Une erreur s'est produite", err },
+			{ status: 400 }
+		);
+	}
+}
---
+++ src/app/api/clients/single/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getClientUnique } from "@/db/actions";
+
+export async function GET(req: NextRequest) {
+   const nomClient = req.nextUrl.searchParams.get("name");
+    
+	try {
+		const client = await getClientUnique(nomClient!)
+        return NextResponse.json({ message: "Client récupéré avec succès !", client }, { status: 200 });
+	} catch (err) {
+		return NextResponse.json(
+			{ message: "Une erreur s'est produite", err },
+			{ status: 400 }
+		);
+	}
+}
